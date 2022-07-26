7021fb6000e8f0e2cfb0c312bbdcf3072bd7a253
Park jinwoo <ceo@chartlab.co.kr>
2022-07-26 16:23:28 +0900

    첫번째 커밋(전체 올리기)

---
+++ src/include/Defines.js
@@ -0,0 +1,8 @@
+const CAR1 = "http://img.danawa.com/cms/img/2014/06/13/1402626434_thumb.jpg";
+const CAR2 = "https://c.wallhere.com/photos/85/46/Tesla_Motors_Tesla_Model_S_car-56926.jpg!d";
+const CAR3 = "https://img.hankyung.com/photo/202104/01.25994984.1.jpg";
+const CAR4 = "https://t1.daumcdn.net/cfile/tistory/9983F83A5B9F450528";
+
+const EMPTY_HEART = "🤍";
+const FILL_HEART = "💖";
+
---
+++ src/utils/Utils.js
@@ -0,0 +1,19 @@
+
+const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
+
+const jsonLocalStorage = {
+    setItem: (key, value) => {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    getItem: (key) => {
+      return JSON.parse(localStorage.getItem(key));
+    },
+  };
+
+  const fetchCat = async (text) => {
+    const OPEN_API_DOMAIN = "https://cataas.com";
+    const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
+    const responseJson = await response.json();
+    return `${OPEN_API_DOMAIN}/${responseJson.url}`;
+  };
+  
